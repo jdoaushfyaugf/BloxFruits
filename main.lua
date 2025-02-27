b514bf7b0b9d3341fbe55f0416022bc57d3eb410
jdoaushfyaugf <164793135+jdoaushfyaugf@users.noreply.github.com>
2025-02-27 13:23:17 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,35 +1,33 @@
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Blox Fruits = Rayfield:CreateWindow({
+   Name = "Blox Fruits - Nebula Hub",
+   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+   LoadingTitle = "Blox fruits - Nebula hub",
+   LoadingSubtitle = "by devs at nebula hub",
+   Theme = "Ocean", -- Check https://docs.sirius.menu/rayfield/configuration/themes
 
-local Window1 = Rayfield:CreateWindow({
-    Name = "Nebula Hub Blox Fruits",
-    Icon = 0,
-    LoadingTitle = "Blox Fruits - Nebula Hub",
-    LoadingSubtitle = "By the devs at Nebula Hub!",
-    Theme = "Ocean",
+   DisableRayfieldPrompts = false,
+   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
 
-    DisableRayfieldPrompts = false,
-    DisableBuildWarnings = false,
-
-    ConfigurationSettings = {
-        Enabled = true,
-        FolderName = "Blox Fruit Script - Nebula Hub",
-        FileName = "Nebula Hub"
-    },
-
-    Discord = {
+   ConfigurationSaving = {
       Enabled = true,
-      Invite = "https://discord.gg/aPWtSVVpF7",
-      RememberJoins = true
-    }
+      FolderName = nil, -- Create a custom folder for your hub/game
+      FileName = "Nebula Hub"
+   },
 
-    KeySystem = true,
-    KeySettings = {
-      Title = "Nebula hub key system",
-      Subtitle = "Made by the devs at Nebula Hub!",
-      Note = "Get key at our discord server!",
-      FileName = "Key",
-      SaveKey = false,
-      GetKeyFromSite = true,
-      Key = "https://pastebin.com/raw/GJXD8xaX"
-    }
+   Discord = {
+      Enabled = true, -- Prompt the user to join your Discord server if their executor supports it
+      Invite = "https://discord.gg/aPWtSVVpF7", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+   },
+
+   KeySystem = true, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "Nebula Hub",
+      Subtitle = "Key System",
+      Note = "Key at discord!", -- Use this to tell the user how to get a key
+      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      GrabKeyFromSite = true, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"https://pastebin.com/raw/GJXD8xaX"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+   }
 })
