ab9dc9e5b19e1d6e28f709f4c300ab6f5430a761
jdoaushfyaugf <164793135+jdoaushfyaugf@users.noreply.github.com>
2025-02-27 13:46:32 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -37,13 +37,13 @@ local Blox Fruits = Rayfield:CreateWindow({
 local PlayerTab = Blox Fruits:CreateTab("Misc", 0)
 
 local Slider = PlayerTab:CreateSlider({
-    Name = "WalkSpeed",
-    Range = {1, 10},
+    Name = "Dash length",
+    Range = {10, 1000},
     Increment = 1,
-    Suffix = "Speed",
+    Suffix = "Length",
     CurrentValue = 10,
-    Flag = "Slider1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+    Flag = "Slider2", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
     Callback = function(Value)
-     game.Players.LocalPlayer.Character:SetAttribute("SpeedMultiplier", Value)
+     game.Players.LocalPlayer.Character:SetAttribute("DashLength", Value)
     end,
  })
